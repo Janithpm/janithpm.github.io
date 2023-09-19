3c840d6dfc3ca97d71334faa904b56b22aaf09b7
Janith Madarasinghe <janithpm9991@gmail.com>
2023-09-19 07:03:42 +0530

    dat.gui added

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GUI } from 'dat.gui'
 
 const loader = new GLTFLoader();
 const scene = new THREE.Scene();
@@ -18,9 +19,9 @@ document.body.appendChild(renderer.domElement);
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
-camera.position.z = 3.5;
-camera.position.y = 0.5;
-camera.position.x = 0;
+camera.position.z = 6;
+camera.position.y = 1.3;
+camera.position.x = -2.6;
 // camera.lookAt(0, 0.5, 0);
 
 // axis helper
@@ -46,16 +47,58 @@ scene.add(directionalLight);
 
 
 //
+const gui = new GUI()
 
 
 //glb loader
-loader.load('./assets/glb/tbody.glb', function (gltf) {
-    gltf.scene.scale.set(1.2, 1.2, 1.2);
-    gltf.scene.position.set(0, -0.7, 0);
-    gltf.scene.rotateX(0.1)
-    // gltf.scene.rotateY(-0.8);
-    scene.add(gltf.scene);
+loader.load('./assets/glb/1.glb', function (gltf) {
+
+    const body = gltf.scene
+
+    body.scale.set(2, 2, 2);
+    body.position.set(0, -0.7, 0);
+    body.rotateX(0.1)
+    body.rotateY(-0.45)
+    body.rotateZ(0.03)
+    scene.add(body);
+
+
+
+    const bodyFolder = gui.addFolder('Body')
+    bodyFolder.add(body.position, 'x', -10, 10)
+    bodyFolder.add(body.position, 'y', -10, 10)
+    bodyFolder.add(body.position, 'z', -10, 10)
+
+    const rotation = gui.addFolder('Body Rotaion')
+    rotation.add(body.rotation, 'x', -Math.Pi * 180, Math.Pi * 180)
+    rotation.add(body.rotation, 'y', -Math.Pi * 180, Math.Pi * 180)
+    rotation.add(body.rotation, 'z', -Math.Pi * 180, Math.Pi * 180)
+
+    const scale = gui.addFolder('Body Scale')
+    scale.add(body.scale, 'x', 0, 10)
+    scale.add(body.scale, 'y', 0, 10)
+    scale.add(body.scale, 'z', 0, 10)
+    scale.open()
+
+
 })
+
+
+const cameraFolder = gui.addFolder('Camera')
+cameraFolder.add(camera.position, 'z', 0, 10)
+cameraFolder.add(camera.position, 'x', -10, 10)
+cameraFolder.add(camera.position, 'y', -10, 10)
+
+// add camera control move to diganonal direction
+cameraFolder.add(camera, 'fov', 0, 180).onChange(() => {
+    camera.updateProjectionMatrix()
+})
+
+
+cameraFolder.open()
+
+
+
 
 
 
