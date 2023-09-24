e9da62dafde3a1822d208a6cabe56703e820167e
Janith Madarasinghe <janithpm9991@gmail.com>
2023-09-25 00:21:15 +0530

    set back

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 
 gsap.registerPlugin(ScrollTrigger);
 
